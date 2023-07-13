06542ea8d8bcc6bca583e73413e2abc3ae184cfd
koreanthinker <koreanthinker@gmail.com>
2023-07-13 16:20:30 +0900

    eslint fix

--- server/src/client.ts
+++ server/src/client.ts
@@ -1,13 +1,19 @@
-import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
-import type { AppRouter } from './router';
+import {createTRPCProxyClient, httpBatchLink} from '@trpc/client';
+import type {AppRouter} from './router';
 
+const client = createTRPCProxyClient<AppRouter>({
+  links: [
+    httpBatchLink({
+      url: 'http://localhost:3000/trpc',
+    }),
+  ],
+});
 
-  const client = createTRPCProxyClient<AppRouter>({
-    links: [
-      httpBatchLink({
-        url: 'http://localhost:3000/trpc',
-      }),
-    ],
-  });
+const test = async () => {
+  const users = await client.user.list.query();
+  // eslint-disable-next-line no-console
+  console.log(users);
+};
 
-client.user.list.query().then((user) => console.log(user))
+// eslint-disable-next-line no-void
+void test();
