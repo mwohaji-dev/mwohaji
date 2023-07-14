070b61a7fda789c66d6c7b50b67e3d2f56c198e0
koreanthinker <koreanthinker@gmail.com>
2023-07-14 18:03:26 +0900

    install skeleton place holder

--- app/App.tsx
+++ app/App.tsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import {Text, Platform, View} from 'react-native';
 import type {AppRouter} from '../server/src/router';
 import {createTRPCReact, httpBatchLink} from '@trpc/react-query';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import Icon1 from 'react-native-vector-icons/MaterialCommunityIcons';
 import Icon2 from 'react-native-vector-icons/MaterialIcons';
+import SkeletonPlaceholder from 'react-native-skeleton-placeholder';
 
 function TestComponent() {
-  const {data} = trpc.hello.greeting.useQuery({name: 'my name'});
+  const [data] = trpc.hello.greeting.useSuspenseQuery({name: 'my name'});
 
   return (
     // eslint-disable-next-line react-native/no-inline-styles
@@ -32,8 +33,8 @@ const trpcClient = trpc.createClient({
     httpBatchLink({
       url:
         Platform.OS === 'android'
-          ? 'http://10.0.2.2:3000/trpc'
-          : 'http://localhost:3000/trpc',
+          ? 'http://10.0.2.2:4000/trpc'
+          : 'http://localhost:4000/trpc',
       // async headers() {
       // return {
       //   authorization: getAuthCookie(),
@@ -48,7 +49,28 @@ export default function App(): JSX.Element {
   return (
     <trpc.Provider client={trpcClient} queryClient={queryClient}>
       <QueryClientProvider client={queryClient}>
-        <TestComponent />
+        <Suspense
+          fallback={
+            <SkeletonPlaceholder borderRadius={4}>
+              <SkeletonPlaceholder.Item flexDirection="row" alignItems="center">
+                <SkeletonPlaceholder.Item
+                  width={60}
+                  height={60}
+                  borderRadius={50}
+                />
+                <SkeletonPlaceholder.Item marginLeft={20}>
+                  <SkeletonPlaceholder.Item width={120} height={20} />
+                  <SkeletonPlaceholder.Item
+                    marginTop={6}
+                    width={80}
+                    height={20}
+                  />
+                </SkeletonPlaceholder.Item>
+              </SkeletonPlaceholder.Item>
+            </SkeletonPlaceholder>
+          }>
+          <TestComponent />
+        </Suspense>
       </QueryClientProvider>
     </trpc.Provider>
   );
