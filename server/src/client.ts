739c4beb070613211731ddbdd093e7bc4e504075
koreanthinker <koreanthinker@gmail.com>
2023-07-16 17:30:41 +0900

    ci fix

--- server/src/client.ts
+++ server/src/client.ts
@@ -6,7 +6,7 @@ import prisma from './configs/prisma';
 const client = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
-      url: 'http://localhost:4000/trpc',
+      url: 'https://server-na5txsvfxq-du.a.run.app/trpc',
     }),
   ],
 });
@@ -14,13 +14,19 @@ const client = createTRPCProxyClient<AppRouter>({
 const test = async () => {
   const greeting = await client.hello.greeting.query({name: 'test name'});
   console.log(greeting);
-  await prisma.user.createMany({
-    data: [
-      {name: 'name1', email: 'email1'},
-      {name: 'name2', email: 'email2'},
-      {name: 'name3', email: 'email3'},
-    ],
-    skipDuplicates: true,
+  await prisma.content.create({
+    data: {
+      googleMapLink: '',
+      hashTag: '',
+      kakaoMapLink: '',
+      latitude: 1,
+      longitude: 1.1,
+      name: 'm',
+      naverMapLink: '',
+      recommandHashTags: [],
+      tMapLink: '',
+      type: 'realtimeHotPlace',
+    },
   });
   const users = await client.user.list.query();
   console.log(users);
