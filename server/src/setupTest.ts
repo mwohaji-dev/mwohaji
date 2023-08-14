7355e5ed2bddb038c7157c161d0a456ab456f2aa
koreanthinker <koreanthinker@gmail.com>
2023-08-15 02:42:44 +0900

    add firebase clear logic

--- server/src/setupTest.ts
+++ server/src/setupTest.ts
@@ -1,6 +1,7 @@
 import {exec as _exec} from 'child_process';
 import {promisify} from 'util';
 import {DATABASE_URL} from './configs/env';
+import {auth} from './configs/firebase';
 
 const exec = promisify(_exec);
 
@@ -8,4 +9,7 @@ beforeAll(async () => {
   // 매 테스트마다 데이터베이스를 초기화합니다.
   const command = `DATABASE_URL=${DATABASE_URL} yarn prisma:initdb`;
   await exec(command);
+  // 매 테스트마다 firebase를 초기화합니다.
+  const users = await auth.listUsers();
+  await auth.deleteUsers(users.users.map(({uid}) => uid));
 });
