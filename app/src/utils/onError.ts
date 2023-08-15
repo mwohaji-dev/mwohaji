bac903bda3880f68667f365653d4a2f533ca6ca3
koreanthinker <koreanthinker@gmail.com>
2023-08-15 21:11:55 +0900

    change onError handler

--- app/src/utils/onError.ts
+++ app/src/utils/onError.ts
@@ -4,10 +4,11 @@ import Toast from 'react-native-toast-message';
 
 export default function onError(error: unknown) {
   if (error instanceof TRPCClientError) {
-    if (error.data.code === 'BAD_REQUEST') {
-      // input이 잘못된 bad request는 TRPCError를 message에 json에 배열로 넣어서 반환해줍니다.
-      // 따라서 파싱후 전부 토스트로 출력해줍니다.
-      const errors = JSON.parse(error.message) as TRPCError[];
+    // input이 잘못된 bad request는 TRPCError를 message에 json에 배열로 넣어서 반환해줍니다.
+    // 따라서 파싱후 전부 토스트로 출력해줍니다.
+    const {message} = error;
+    if (message.startsWith('[') && message.endsWith(']')) {
+      const errors = JSON.parse(message) as TRPCError[];
       errors.forEach(err =>
         Toast.show({
           type: 'error',
@@ -17,7 +18,7 @@ export default function onError(error: unknown) {
     } else {
       Toast.show({
         type: 'error',
-        text1: error.message,
+        text1: message,
       });
     }
   }
